d5031537fbd24b3bcde0dc806cde93d0d474c5d8
chapin <chapin@class100.com>
2019-02-15 14:37:50 +0800

    extract common code

--- CLI_test.go
+++ CLI_test.go
@@ -27,15 +27,3 @@ func TestCLI(t *testing.T) {
 		assertPlayerWin(t, playerStore, "Cleo")
 	})
 }
-
-func assertPlayerWin(t *testing.T, store *StubPlayerStore, winner string) {
-	t.Helper()
-
-	if len(store.winCalls) != 1 {
-		t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
-	}
-
-	if store.winCalls[0] != winner {
-		t.Errorf("did not store correct winner got '%s' want '%s'", store.winCalls[0], winner)
-	}
-}
